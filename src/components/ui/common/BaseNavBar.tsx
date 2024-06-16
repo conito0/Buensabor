1c474b40d23072c29fb19d45b6022afb2fbf5e0f
roAscurra <rominaascurra4887@gmail.com>
2024-06-16 14:33:54 -0300

    Modificacion para que funcione en local

--- src/components/ui/common/BaseNavBar.tsx
+++ src/components/ui/common/BaseNavBar.tsx
@@ -59,7 +59,7 @@ export const BaseNavBar = () => {
         </div>
         <form className="d-flex align-items-center text-white" role="search">
           { isAuthenticated ? <div className={"px-2"}><img alt={"Imagen de perfil"} className={"rounded-4"} height={20} width={20} src={user?.picture}/><span> {user?.name}</span></div> : <></> }
-          <button hidden={isAuthenticated} onClick={handleLogin} type="button" className={"btn btn-light"}>Registro / Login</button>
+          <button hidden={isAuthenticated} onClick={handleLogin} type="button" className={"btn btn-light"}>Registrarse</button>
           <button hidden={!isAuthenticated}  onClick={handleLogout} type="button" className={"btn btn-light"}>Logout</button>
         </form>
       </div>
